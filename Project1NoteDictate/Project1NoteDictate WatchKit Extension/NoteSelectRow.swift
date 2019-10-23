61dd10f4c9a5a89bc5db520083d0e4ade7635137
Kevin Tanner <kevindavidtanner@gmail.com>
2019-10-23 17:06:58 -0600

    Sets up table, label, and button

---
+++ Project1NoteDictate/Project1NoteDictate WatchKit Extension/NoteSelectRow.swift
@@ -0,0 +1,14 @@
+//
+//  NoteSelectRow.swift
+//  Project1NoteDictate WatchKit Extension
+//
+//  Created by Kevin Tanner on 10/23/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import WatchKit
+
+class NoteSelectRow: NSObject {
+
+    @IBOutlet weak var textLabel: WKInterfaceLabel!
+}
