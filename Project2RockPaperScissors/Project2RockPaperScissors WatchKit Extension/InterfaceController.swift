b92d9e91f746927d88d29f852cb7e8ae6b594152
Kevin Tanner <kevindavidtanner@gmail.com>
2019-10-24 17:41:24 -0600

    Adds game functionality

--- Project2RockPaperScissors/Project2RockPaperScissors WatchKit Extension/InterfaceController.swift
+++ Project2RockPaperScissors/Project2RockPaperScissors WatchKit Extension/InterfaceController.swift
@@ -23,12 +23,26 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var levelCounter: WKInterfaceLabel!
     @IBOutlet weak var timer: WKInterfaceTimer!
     
+    @IBOutlet weak var result: WKInterfaceLabel!
+    
+    // MARK: - Properties
+    
+    var allMoves = ["rock", "paper", "scissors"]
+    var shouldWin = true
+    var level = 1
+    
     // MARK: - Lifecycle Methods
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        rock.setBackgroundImage(UIImage(named: "rock"))
+        paper.setBackgroundImage(UIImage(named: "paper"))
+        scissors.setBackgroundImage(UIImage(named: "scissors"))
+        
+        timer.start()
+        newLevel()
     }
     
     override func willActivate() {
@@ -44,12 +58,63 @@ class InterfaceController: WKInterfaceController {
     // MARK: - Actions
 
     @IBAction func rockTapped() {
+        if shouldWin {
+            check(for: "scissors")
+        } else {
+            check(for: "paper")
+        }
     }
     
     @IBAction func paperTapped() {
+        if shouldWin {
+            check(for: "rock")
+        } else {
+            check(for: "scissors")
+        }
     }
     
     @IBAction func scissorsTapped() {
+        if shouldWin {
+            check(for: "paper")
+        } else {
+            check(for: "rock")
+        }
+    }
+    
+    // MARK: - Custom Methods
+    
+    func newLevel() {
+        if level == 21 {
+            result.setHidden(false)
+            question.setHidden(true)
+            answers.setHidden(true)
+            timer.stop()
+            return
+        }
+        
+        levelCounter.setText("\(level)/20")
+        
+        if Bool.random() {
+            setTitle("Win!")
+            shouldWin = true
+        } else {
+            setTitle("Lose!")
+            shouldWin = false
+        }
+        
+        allMoves.shuffle()
+        question.setImage(UIImage(named: allMoves[0]))
+    }
+
+    func check(for answer: String) {
+        if allMoves[0] == answer {
+            level += 1
+            newLevel()
+        } else {
+            level -= 1
+            if level < 1 { level = 1 }
+            newLevel()
+        }
     }
     
 }
