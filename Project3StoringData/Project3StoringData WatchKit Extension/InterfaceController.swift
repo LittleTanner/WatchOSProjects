2a2b2a8c80b56620b93aa3d0341a606d32244e72
Kevin Tanner <kevindavidtanner@gmail.com>
2019-10-28 19:25:16 -0700

    Added three different ways to save and read data, userDefaults, keychain, filesystem.

--- Project3StoringData/Project3StoringData WatchKit Extension/InterfaceController.swift
+++ Project3StoringData/Project3StoringData WatchKit Extension/InterfaceController.swift
@@ -16,6 +16,14 @@ class InterfaceController: WKInterfaceController {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        testUserDefaults()
+        testUserDefaults()
+        
+        testKeychain()
+        testKeychain()
+        
+        testFile()
+        testFile()
     }
     
     override func willActivate() {
@@ -28,4 +36,49 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    func testUserDefaults() {
+        // get hold of our app's user defaults
+        let defaults = UserDefaults.standard
+        
+        if let contents = defaults.string(forKey: "shared_default") {
+            // saved data was found!
+            print("Reading from defaults")
+            print(contents)
+        } else {
+            // no saved data - create it!
+            print("Writing to defaults")
+            defaults.set("This is the saved default", forKey: "shared_default")
+        }
+    }
+    
+    
+    
+    func testKeychain() {
+        let keychain = KeychainWrapper.standard
+        
+        if let contents = keychain.string(forKey: "shared_defaults") {
+            print("Reading from keychain")
+            print(contents)
+        } else {
+            print("Writing to keychain")
+            keychain.set("This is the saved keychain.", forKey: "shared_defaults")
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func testFile() {
+        let saveURL = getDocumentsDirectory().appendingPathComponent("shared_file")
+        
+        if let contents = try? String(contentsOf: saveURL) {
+            print("Reading from file")
+            print(contents)
+        } else {
+            print("Writing to file")
+            try? "This is the saved file".write(to: saveURL, atomically: true, encoding: .utf8)
+        }
+    }
 }
