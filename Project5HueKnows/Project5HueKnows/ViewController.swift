e33dae44a42efdbca2c264d5f84971cb8d54aab6
Kevin Tanner <kevindavidtanner@gmail.com>
2019-10-31 14:40:41 -0700

    Initial setup for project 5

---
+++ Project5HueKnows/Project5HueKnows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5HueKnows
+//
+//  Created by Kevin Tanner on 10/31/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
