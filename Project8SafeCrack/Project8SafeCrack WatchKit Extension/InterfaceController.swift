d575204dc0c8fd6d7d48327cb8db701d3e55bf9f
Kevin Tanner <kevindavidtanner@gmail.com>
2019-11-06 13:55:06 -0800

    Finishes up safe crack game

--- Project8SafeCrack/Project8SafeCrack WatchKit Extension/InterfaceController.swift
+++ Project8SafeCrack/Project8SafeCrack WatchKit Extension/InterfaceController.swift
@@ -23,6 +23,8 @@ class InterfaceController: WKInterfaceController, WKCrownDelegate {
     
     var currentSafeValue: Float = 50
     var targetSafeValue = 0
+    var allSafeNumbers = [Int]()
+    var correctValues = [String]()
 
     
     // MARK: - Lifecycle Methods
@@ -30,7 +32,7 @@ class InterfaceController: WKInterfaceController, WKCrownDelegate {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        startNewGame()
     }
     
     override func willActivate() {
@@ -51,6 +53,22 @@ class InterfaceController: WKInterfaceController, WKCrownDelegate {
     // MARK: - Actions
 
     @IBAction func nextTapped() {
+        guard Int(currentSafeValue) == targetSafeValue else { return }
+        
+        correctValues.append(String(targetSafeValue))
+        
+        numbersLabel.setText(correctValues.joined(separator: ", "))
+        
+        if correctValues.count == 3 {
+            timer.stop()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                let playAgain = WKAlertAction(title: "Play again", style: .default) { self.startNewGame() }
+                self.presentAlert(withTitle: "You win!", message: nil, preferredStyle: .alert, actions: [playAgain])
+            }
+        } else {
+            pickNumber()
+        }
     }
     
     // MARK: - Custom Methods
@@ -60,7 +78,53 @@ class InterfaceController: WKInterfaceController, WKCrownDelegate {
         currentSafeValue = min(max(0, currentSafeValue), 100)
         safeValue.setValue(currentSafeValue)
         nextButton.setTitle("Enter \(Int(currentSafeValue))")
+        
+        if Int(currentSafeValue) == targetSafeValue {
+            // This is the right number - tap their wrist
+            WKInterfaceDevice.current().play(.click)
+
+            // Now update the UI to show this is good
+            safeValue.setColor(UIColor.green)
+            numbersLabel.setTextColor(UIColor.green)
+            nextButton.setEnabled(true)
+        } else {
+            
+            // Wrong number; make the UI Show this is bad
+            numberIsWrong()
+        }
+    }
+    
+    func startNewGame() {
+        // Reset and start the timer
+        timer.setDate(Date())
+        timer.start()
+        
+        // Create an array of random numbers from 0 to 100
+        allSafeNumbers = Array(0...100)
+        allSafeNumbers.shuffle()
+        
+        // Reset the current Value
+        currentSafeValue = 50
+        safeValue.setValue(50)
+        
+        // Remove all their previous answers and reset the text
+        correctValues.removeAll()
+        numbersLabel.setText("Safe Crack")
+        
+        // Pick the first number to guess
+        pickNumber()
+    }
+    
+    func pickNumber() {
+        targetSafeValue = allSafeNumbers.removeFirst()
+        print(targetSafeValue)
+        numberIsWrong()
     }
 
+    func numberIsWrong() {
+        safeValue.setColor(UIColor.red)
+        numbersLabel.setTextColor(UIColor.white)
+        nextButton.setEnabled(false)
+    }
     
 }
