eaa1fe8c1fc72f5e8077498084982da130fc4286
Kevin Tanner <kevindavidtanner@gmail.com>
2019-11-06 12:42:30 -0800

    Sets up initial project for project 8

---
+++ Project8SafeCrack/Project8SafeCrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8SafeCrack
+//
+//  Created by Kevin Tanner on 11/6/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
