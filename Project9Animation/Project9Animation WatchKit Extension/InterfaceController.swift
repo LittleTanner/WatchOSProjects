67b2e95a7841140f40abd7a7b7c6fe86bcc09571
Kevin Tanner <kevindavidtanner@gmail.com>
2019-11-06 19:36:37 -0800

    Added attribute animations

--- Project9Animation/Project9Animation WatchKit Extension/InterfaceController.swift
+++ Project9Animation/Project9Animation WatchKit Extension/InterfaceController.swift
@@ -16,6 +16,10 @@ class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var image: WKInterfaceImage!
     
+    // MARK: - Properties
+    
+    var animation = 1
+    
     
     // MARK: - Lifecycle Methods
 
@@ -38,6 +42,36 @@ class InterfaceController: WKInterfaceController {
     // MARK: - Actions
     
     @IBAction func animateTapped() {
+        animate(withDuration: 1) {
+            switch self.animation {
+            case 1:
+                self.image.setAlpha(0.5)
+            case 2:
+                self.image.setVerticalAlignment(.top)
+                self.image.setHorizontalAlignment(.right)
+            case 3:
+                self.image.setRelativeWidth(0.1, withAdjustment: 0)
+                self.image.setRelativeHeight(0.1, withAdjustment: 0)
+            case 4:
+                self.image.setWidth(25)
+                self.image.setHeight(25)
+            case 5:
+                self.image.setTintColor(UIColor.red)
+            case 6:
+                self.image.setTintColor(nil)
+                self.image.setAlpha(1)
+                self.image.setVerticalAlignment(.center)
+                self.image.setHorizontalAlignment(.center)
+                self.image.sizeToFitWidth()
+                self.image.sizeToFitHeight()
+            default:
+                self.animation = 1
+                self.animateTapped()
+                return
+            }
+            
+            self.animation += 1
+        }
     }
     
 
