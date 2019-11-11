21f7d74679ce2a2fcfcafe2e9cfac6db061695bb
Kevin Tanner <kevindavidtanner@gmail.com>
2019-11-11 13:04:03 -0800

    Finishes up and implements game play

--- Project11ColorSpin/Project11ColorSpin WatchKit Extension/InterfaceController.swift
+++ Project11ColorSpin/Project11ColorSpin WatchKit Extension/InterfaceController.swift
@@ -8,6 +8,7 @@
 
 import WatchKit
 import Foundation
+import SpriteKit
 
 
 class InterfaceController: WKInterfaceController {
@@ -48,9 +49,17 @@ class InterfaceController: WKInterfaceController {
     // MARK: - Actions
     
     @IBAction func startGame(_ sender: Any) {
+        
+        if gameScene != nil {
+            guard gameScene.isPlayerAlive == false else { return }
+        }
+        
         gameScene = GameScene(size: CGSize(width: 154, height: 174))
+        gameScene.parentInterfaceController = self
         gameScene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        gameInterface.presentScene(gameScene)
+        
+        let transition = SKTransition.doorway(withDuration: 1)
+        gameInterface.presentScene(gameScene, transition: transition)
     }
     
 
