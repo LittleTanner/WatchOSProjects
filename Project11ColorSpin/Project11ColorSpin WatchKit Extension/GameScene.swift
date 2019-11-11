2a27bb3f228e3d31f6b233b4f7170c915fde9a72
Kevin Tanner <kevindavidtanner@gmail.com>
2019-11-11 11:28:04 -0800

    Sets up initial UI and outlets and actions

---
+++ Project11ColorSpin/Project11ColorSpin WatchKit Extension/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Project11ColorSpin WatchKit Extension
+//
+//  Created by Kevin Tanner on 11/11/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import SpriteKit
+import WatchKit
+
+class GameScene: SKScene {
+
+    override func sceneDidLoad() {
+        backgroundColor = UIColor.blue
+    }
+}
