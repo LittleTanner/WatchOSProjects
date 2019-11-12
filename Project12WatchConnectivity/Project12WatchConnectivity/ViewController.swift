9a9709251a70c2165d3c87b97011d9afae66ce24
Kevin Tanner <kevindavidtanner@gmail.com>
2019-11-12 11:44:18 -0800

    Sets up initial project files

---
+++ Project12WatchConnectivity/Project12WatchConnectivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project12WatchConnectivity
+//
+//  Created by Kevin Tanner on 11/12/19.
+//  Copyright © 2019 Kevin Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
